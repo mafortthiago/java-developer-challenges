9314cbfd9b824987a5d1e2baff8053fdf9e17b55
mafortthiago <thmafort@gmail.com>
2024-07-01 15:28:59 -0300

    feat: Add gym project

---
+++ academia-digital-master/src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -0,0 +1,48 @@
+package me.dio.academia.digital.controller;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.entity.form.AlunoUpdateForm;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+  @Autowired
+  private AlunoServiceImpl service;
+
+  @PostMapping
+  public Aluno create(@Valid @RequestBody AlunoForm form) {
+    return service.create(form);
+  }
+
+  @GetMapping("/{id}")
+  public Aluno get(@PathVariable Long id) {
+    return service.get(id);
+  }
+
+  @GetMapping
+  public List<Aluno> getAll(@RequestParam(value = "dataDeNascimento", required = false)
+                            String dataDeNacimento){
+    return service.getAll(dataDeNacimento);
+  }
+
+  @PutMapping("/{id}")
+  public Aluno update(@PathVariable Long id, @RequestBody AlunoUpdateForm form) {
+    return service.update(id, form);
+  }
+
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Long id) {
+    service.delete(id);
+  }
+
+}
+
---
+++ academia-digital-master/src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaUpdateForm.java
@@ -0,0 +1,11 @@
+package me.dio.academia.digital.entity.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AvaliacaoFisicaUpdateForm {
+  private double peso;
+  private double altura;
+}
